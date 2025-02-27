004b8596164f2f12dfee011079925c7cedc63c16
Luis <120034783@upq.edu.mx>
2025-02-26 21:50:00 -0600

    Clase 26

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def index():  # put application's code here
     return render_template('index.html', students=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
